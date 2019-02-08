b941491b8d1fe8272532e1ad5eb69864d658d80d
Lisa Scott <lrscot@aol.com>
2019-02-08 08:10:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(number)
   (2...number).to_a.each do |divisor|
-    if number % divisor == 0
-      return false
-    end
     if number < 0
       return false
     end 
+    if number % divisor == 0
+      return false
+    end
 end
 true
 end
